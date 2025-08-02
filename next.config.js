e25ae9dd0216d150f79b1765cc02c142b3c3fd1d
Cristian Ruiz JR <cristianruizjr@cristians-mbp.lan>
2025-08-02 18:57:08 -0400

    Add GitHub Actions workflow for Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
   env: {
     CUSTOM_KEY: process.env.CUSTOM_KEY,
   },
